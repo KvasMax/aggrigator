e9060c750aca0119d02b9f04180af2254b0480f7
Maxim Kvasov <maksim@voodoo.rocks>
2019-02-16 17:05:38 +0200

    Add a draft of the project with migration of gradle scripts to kotlin dsl

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+buildscript {
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:3.5.0-alpha03")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.21")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+}
+
+
+tasks {
+    val clean by registering(Delete::class) {
+        delete(buildDir)
+    }
+}
+
